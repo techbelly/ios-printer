3dd70f65fd2c2ea63bd85ae87e5bb3483fbbb0f4
Ben Griffiths <bengriffiths@gmail.com>
2012-04-13 23:09:42 +0100

    Still trying to make this more comprehensible.

--- GFRPrinter/TBMainViewController.m
+++ GFRPrinter/TBMainViewController.m
@@ -53,6 +53,8 @@
 
 - (UIImage *)imageFromData:(NSData *)data
 {
+    
+    // Parse the header to get height and width
     union {
         char      bytes [4];
         uint16_t  values[2];
@@ -61,26 +63,63 @@
     const char * bytes = [data bytes];
     int src_headerSize = sizeof header.bytes;
     memcpy(header.bytes, bytes, src_headerSize);
+    
     uint16_t src_width  = header.values[0];
     uint16_t src_height = header.values[1];
     
-    int src_bitsPerPixel = 8;
+    int src_pixelsPerByte = 8;
+    
+    // Create the destination byte array
     
     int dest_padding = 40;
     
-    int dest_width =  (src_width  + dest_padding);
-    int dest_height = (src_height + dest_padding);
+    int dest_width =  src_width  + dest_padding;
+    int dest_height = src_height + dest_padding;
     
     int dest_bytesPerPixel = 4;
+
+        // (next two lines are the crux of it. src has many pixels per byte,
+        // and dest has many bytes per pixel. Ho hum.)
+    int src_bytesPerRow =  src_width  / src_pixelsPerByte;
     int dest_bytesPerRow = dest_bytesPerPixel * dest_width;
-    int dest_bytesNeeded = dest_height * dest_bytesPerRow;
-
+ 
+    int dest_bytesNeeded = dest_bytesPerRow   * dest_height;
+    
     unsigned char *dest = malloc(dest_bytesNeeded);
-
+    
     // make the background all papery.
     for(int i = 0; i < dest_bytesNeeded; i += 4) {
         [self lightpixel:dest at:i];
     }
+    
+    
+    // write the src bits into the dest array
+    
+    char byte;
+    int src_bytesAcross, dest_bytesAcross, src_bytesDown , dest_bytesDown;
+    
+    int dest_topPadding = dest_padding/2;
+    int dest_leftPadding = dest_padding/2;
+    
+    for (int pixelsDown = 0; pixelsDown < src_height; pixelsDown ++) {
+        
+        src_bytesDown  = pixelsDown * src_bytesPerRow + src_headerSize;
+        dest_bytesDown = (pixelsDown + dest_topPadding) * dest_bytesPerRow;
+        
+        for (int pixelsAcross = 0; pixelsAcross < src_width; pixelsAcross ++) {
+            
+            src_bytesAcross = pixelsAcross / src_pixelsPerByte;
+            byte = bytes[src_bytesDown + src_bytesAcross];
+            
+            int bit = (pixelsAcross % 8);
+            if ((byte & (1 << (7 - bit))) > 0) {            
+                dest_bytesAcross = (pixelsAcross + dest_leftPadding) * dest_bytesPerPixel;
+                [self darkpixel:dest at:dest_bytesDown + dest_bytesAcross];
+            } 
+        }
+    }
+    
+    // make a UIImage out of it.
     
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
     
@@ -93,36 +132,11 @@
                                                  colorSpace,
                                                  kCGImageAlphaPremultipliedLast);
     
-    char byte;
-    int padded_column, padded_row, src_index, dest_index;
-    
-    // next two lines are the crux of it. src has many pixels per byte,
-    // and dest has many bytes per pixel. Ho hum.
-    int src_rowsize =  (src_width/src_bitsPerPixel);
-    int dest_rowsize = dest_width * dest_bytesPerPixel;
-
-    int dest_src_offset = dest_padding/2;
-    
-    for (int row = 0; row < src_height; row ++) {
-        for (int column = 0; column < src_width; column ++) {
-
-            src_index = (column/src_bitsPerPixel) + (row * src_rowsize) + src_headerSize;
-            byte = bytes[src_index];
-
-            padded_column = column + dest_src_offset;
-            padded_row    = row    + dest_src_offset;
-            
-            dest_index = padded_column * dest_bytesPerPixel + padded_row * dest_rowsize;
-                        
-            int bit = (column % 8);
-            if ((byte & (1 << (7 - bit))) > 0) {
-                [self darkpixel:dest at:dest_index];
-            } 
-        }
-    }
-    
     CGImageRef cgimage = CGBitmapContextCreateImage(context); 
     UIImage *image =  [UIImage imageWithCGImage:cgimage scale:1.0 orientation:UIImageOrientationDown];
+    
+    // tidy up and go home.
+    
     free(dest);
     return image;
 }
